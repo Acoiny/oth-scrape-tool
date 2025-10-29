fb2e3fe895e7c603aef636e139af17eac7de8954
Acoiny <tim.porbadnigk@gmx.de>
2025-10-29 17:24:21 +0100

    implemented image retrieval for markdown

--- mensaplan.py
+++ mensaplan.py
@@ -8,56 +8,60 @@ from bs4 import BeautifulSoup
 
 class Meal:
     def __init__(self, name: str,
-                 kennzeichnung: str,
-                 price_students: str,
-                 price_workers: str,
-                 price_guest: str,
+                 # kennzeichnung: str,
+                 prices: list[str],
                  image_url: str) -> None:
         self.name = name
-        self.kennzeichnung = kennzeichnung
-        self.price_students = float(price_students.replace(',', '.'))
-        self.price_workers = float(price_workers.replace(',', '.'))
-        self.price_guest = float(price_guest.replace(',', '.'))
+        # self.kennzeichnung = kennzeichnung
+        self.price_students = prices[0].replace(',', '.')
+        self.price_workers = prices[1].replace(',', '.')
+        self.price_guest = prices[2].replace(',', '.')
         self.image_url = image_url
     def __str__(self) -> str:
-        return f'{self.name} - {self.kennzeichnung}: {self.price_students:.2f}€'
+        return f'{self.name} - : {self.price_students}'
+
+    def to_markdown(self) -> str:
+        image_format = f'<img src="{self.image_url}" width="200"/>'
+        return f'{self.name} - : {self.price_students}\n{image_format}'
 
 class Weekday:
-    def __init__(self, datum: str) -> None:
-        self.datum = dt.datetime.strptime(datum, "%d.%m.%Y").date()
+    def __init__(self, datum: dt.date) -> None:
+        # self.datum = dt.datetime.strptime(datum, "%d.%m.%Y").date()
+        self.datum = datum.strftime('%d.%m.%Y')
         self.suppen: list[Meal] = []
         self.hauptspeisen: list[Meal] = []
         self.beilagen: list[Meal] = []
         self.nachspeisen: list[Meal] = []
 
     def add_meal(self, meal: Meal, meal_type: str) -> None:
-        if meal_type.startswith('HG'):
-            self.hauptspeisen.append(meal)
-        elif meal_type.startswith('B'):
-            self.beilagen.append(meal)
-        elif meal_type.startswith('Suppe'):
-            self.suppen.append(meal)
-        elif meal_type.startswith('N'):
-            self.nachspeisen.append(meal)
-        else:
-            raise Exception(f'Unknown meal type: {meal_type}')
+        match meal_type.lower():
+            case 'hauptgerichte':
+                self.hauptspeisen.append(meal)
+            case 'beilagen':
+                self.beilagen.append(meal)
+            case 'suppen':
+                self.suppen.append(meal)
+            case 'nachspeisen':
+                self.nachspeisen.append(meal)
+            case _:
+                raise Exception(f'Unknown meal type: {meal_type}')
 
     def to_markdown_str(self) -> str:
         res = '## Suppen\n'
         for su in self.suppen:
-            res += f'- {su}\n'
+            res += f'- {su.to_markdown()}\n'
         res += '## Beilagen\n'
         for vs in self.beilagen:
-            res += f'- {vs}\n'
+            res += f'- {vs.to_markdown()}\n'
         res += '## Hauptspeisen\n'
         for hs in self.hauptspeisen:
-            res += f'- {hs}\n'
+            res += f'- {hs.to_markdown()}\n'
         res += '## Nachspeisen\n'
         for ns in self.nachspeisen:
-            res += f'- {ns}\n'
+            res += f'- {ns.to_markdown()}\n'
 
         return res
-    
+
     def __str__(self) -> str:
         res = '    Suppen:\n'
 
@@ -80,110 +84,128 @@ class Mensaplan:
     Gets the current mensaplan and handles stringifying it and
     formatting as markdown
     """
-    def __init__(self, calendar_week: int | None, today: bool) -> None:
-        # always use the current week if today is set
-        # if it is not set, then allow a custom calendar week
-        if today or calendar_week == None: calendar_week = dt.date.today().isocalendar()[1]
-        self.today = today
-        self.url = f'https://www.stwno.de/infomax/daten-extern/csv/HS-R-tag/{calendar_week}.csv'
-        self.url_images = f'https://stwno.de/infomax/daten-extern/html/speiseplan-render.php'
-        self.days: dict[str, Weekday] = {}
+    def __init__(self, calendar_week: int | None = None, today: bool = False) -> None:
+
+        # if no week is specified, use the current week
+        if calendar_week == None:
+            calendar_week = dt.datetime.today().isocalendar()[1]
+
+        # we get a list of all dates and cache it
+        self.weekdays = self.get_weekdays(calendar_week)
+
+        # by default, we assume that we want to get all the days of the selected week
+        self.dates: list[dt.date] = self.weekdays
+
+        # if we only want today, we replace all days by the current date
+        if today:
+            self.dates = [ dt.datetime.today() ]
+
+        self.url = 'https://stwno.de/infomax/daten-extern/html/speiseplan-render.php'
+        self.days: dict[dt.date, Weekday] = {}
+
+    def get_weekdays(self, week) -> list[dt.date]:
+        """
+        Returns all the dates of the current week
+        """
+        first_day_of_year = dt.date(dt.datetime.today().year, 1, 1)
+        first_monday = first_day_of_year + dt.timedelta(weeks=week - 1, days=-first_day_of_year.weekday())
+        # count from monday to friday
+        weekdays = [first_monday + dt.timedelta(days=i) for i in range(5)]
+        return weekdays
+
+    def select_single_weekday(self, day: int) -> None:
+        """
+        Adds another day to request the mensaplan for
+        """
+        self.dates = [ self.weekdays[day] ]
 
     def get(self) -> None:
         """
-        Fetches the current mensaplan and stores it
+        Fetches all the data for all added days
         """
-        with urlopen(self.url) as url:
-            lines = [l.decode('latin-1').strip() for l in url.readlines()]
-            csv_reader = csv.reader(lines, delimiter=';')
+        for day in self.dates:
+            self.get_with_images(day)
 
-            # discard fields
-            next(csv_reader)
-
-            for row in csv_reader:
-                day = self.days.get(row[1])
-                if day == None:
-                    day = Weekday(row[0])
-                    self.days[row[1]] = day
-                meal = Meal(row[3], row[4], row[6], row[7], row[8]) # pyright: ignore
-                day.add_meal(meal, row[2])
-
-    def get_with_images(self) -> None:
+    def get_with_images(self, date: dt.date) -> None:
         """
-        Fetches the current mensaplan with all it's images
+        Fetches the selected day's mensaplan with all it's images
         """
         data = {
-                "date": ASDAS,
+                "date": date.strftime('%Y-%m-%d'),
                 "func": "make_spl",
                 "lang": "de",
                 "locId": "HS-R-tag",
                 "w": ""
         }
 
-        res = requests.post("https://stwno.de/infomax/daten-extern/html/speiseplan-render.php", data=data)
+        # get the base url for images
+        base_url: str = self.url[:self.url.rindex('/') + 1]
+
+        res = requests.post(self.url, data=data).content
+        soup = BeautifulSoup(res.decode('utf-8'), 'html.parser')
+        headers = soup.find_all('tr')
+
+        day = self.days.get(date)
+        if day == None:
+            day = Weekday(date)
+            self.days[date] = day
+
+        current_meal_type: str = ''
+
+        for header in headers:
+            cl = header.attrs.get('class')
+            if not cl: continue
+
+            if 'gruppenkopf' in cl:
+                current_meal_type = header.find('td').get_text() # type: ignore
+            if 'essenzeile' in cl:
+                contents = header.find_all('td')
+                preise: list = [ el.get_text() for el in header.find_all('td', class_='preis') ]
+                name = contents[1].get_text()
+                img_url: str = ''
+                if img := contents[0].find('img'):
+                    img_url = base_url + img['src'] # type: ignore
+                day.add_meal(Meal(name, preise, img_url), current_meal_type)
 
 
     def to_markdown_str(self) -> str:
         res = ''
-
-        keys_to_names = [
-            ('Mo', 'Montag'),
-            ('Di', 'Dienstag'),
-            ('Mi', 'Mittwoch'),
-            ('Do', 'Donnerstag'),
-            ('Fr', 'Freitag'),
-            ('Sa', 'Samstag'),
-            ('So', 'Sonntag')
+        weekdays = [
+            'Montag',
+            'Dienstag',
+            'Mittwoch',
+            'Donnerstag',
+            'Freitag',
+            'Samstag',
+            'Sonntag'
         ]
-
-        if self.today:
-            index = dt.date.today().isocalendar()[2] - 1
-            key, name = keys_to_names[index]
-            res += f'# {name}\n'
-
-            day = self.days.get(key, None)
-
-            if day == None: res += 'No mensaplan for today!\n'
-            else: res += f'{day.to_markdown_str()}'
-        else:
-            for key, name in keys_to_names[:-2]:
-                try:
-                    res += f'# {name}\n'
-                    res += f'{self.days[key].to_markdown_str()}'
-                except:
-                    res += f'> [!WARNING]\n> No mensaplan!\n'
+        for d in self.days:
+            day = self.days[d]
+            try:
+                res += f'# {weekdays[d.weekday()]} ({d.strftime('%d.%m.%Y')})\n'
+                res += day.to_markdown_str() + '\n'
+            except Exception as e:
+                res += '> [!WARNING]\n> No mensaplan!\n'
 
         return res
-
-    def stringify_day(self, index: int) -> str:
-        keys_to_names = [
-            ('Mo', 'Montag'),
-            ('Di', 'Dienstag'),
-            ('Mi', 'Mittwoch'),
-            ('Do', 'Donnerstag'),
-            ('Fr', 'Freitag'),
-            ('Sa', 'Samstag'),
-            ('So', 'Sonntag')
-        ]
-
-        try:
-            return f'{keys_to_names[index][1]}:\n{self.days[keys_to_names[index][0]]}\n'
-        except:
-            return f'No mensaplan for {keys_to_names[index][1]}\n'
 
     def __str__(self) -> str:
         res = ''
-
-        if self.today:
-            index = dt.date.today().isocalendar()[2] - 1
-            res += self.stringify_day(index)
-        else:
-            for i in range(5):
-                res += self.stringify_day(i)
-
+        weekdays = [
+            'Montag',
+            'Dienstag',
+            'Mittwoch',
+            'Donnerstag',
+            'Freitag',
+            'Samstag',
+            'Sonntag'
+        ]
+        for day in self.days:
+            res += f'{weekdays[day.weekday()]}:\n'
+            res += str(self.days[day]) + '\n'
         return res
 
 if __name__ == '__main__':
-    m = Mensaplan(None, False)
+    m = Mensaplan(today=True)
     m.get()
     print(m)
--- oth_tool.py
+++ oth_tool.py
@@ -5,12 +5,15 @@ from mensaplan import Mensaplan
 
 import argparse
 
+import datetime as dt
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('command', help='The action to execute, either mensaplan or blackboard', type=str)
 parser.add_argument('-m', '--markdown', help='Format the output as markdown', action='store_true')
 parser.add_argument('-d', '--weekday', help='Only retrieves plan of DAY (mensaplan only)', metavar='DAY', type=str)
-parser.add_argument('-w', '--calendar_week', help='Retrieves plan for calendar week WEEK (mensaplan only)', metavar='WEEK', type=int)
+parser.add_argument('-w', '--calendar_week', help='Retrieves plan for calendar week WEEK (mensaplan only). \
+                                                   This may result in empty output if the week is in the past or too far into the future.', metavar='WEEK', type=int)
 parser.add_argument('-t', '--today', help='Show the mensaplan for today (mensaplan only)', action='store_true')
 
 args = parser.parse_args()
@@ -25,28 +28,33 @@ def main() -> None:
         else:
             print(blackboard)
     elif args.command in ('mensaplan', 'mensa', 'me', 'm'):
-        mensa = Mensaplan(args.calendar_week, args.today)
-        mensa.get()
 
+
+        week = args.calendar_week
+
+        mensa = Mensaplan(week, args.today)
+
+        if args.weekday != None:
+            day = args.weekday.lower()
+            if day in ('monday', 'montag', 'mo', 'm'):
+                mensa.select_single_weekday(0)
+            elif day in ('tuesday', 'dienstag', 'tu', 'di'):
+                mensa.select_single_weekday(0)
+            elif day in ('wednesday', 'mittwoch', 'wed', 'mi', 'w', 'm'):
+                mensa.select_single_weekday(0)
+            elif day in ('thursday', 'donnerstag', 'th', 'do'):
+                mensa.select_single_weekday(0)
+            elif day in ('friday', 'freitag', 'fr', 'f'):
+                mensa.select_single_weekday(0)
+            else:
+                parser.exit(1, f'Invalid weekday "{args.weekday}"\n')
+
+        mensa.get()
         if args.markdown:
             print(mensa.to_markdown_str())
         else:
-            if args.weekday != None:
-                day = args.weekday.lower()
-                if day in ('monday', 'montag', 'mo', 'm'):
-                    print(mensa.stringify_day(0))
-                elif day in ('tuesday', 'dienstag', 'tu', 'di'):
-                    print(mensa.stringify_day(1))
-                elif day in ('wednesday', 'mittwoch', 'wed', 'mi', 'w', 'm'):
-                    print(mensa.stringify_day(2))
-                elif day in ('thursday', 'donnerstag', 'th', 'do'):
-                    print(mensa.stringify_day(3))
-                elif day in ('friday', 'freitag', 'fr', 'f'):
-                    print(mensa.stringify_day(4))
-                else:
-                    parser.exit(1, f'Invalid weekday "{args.weekday}"\n')
-            else:
-                print(mensa)
+            print(mensa)
+
     else:
         parser.exit(1,parser.format_help())
 
